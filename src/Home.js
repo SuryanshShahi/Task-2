77a1fb8154d3489debf9cc950be85b23c96a526f
SuryanshShahi <suryansh06shahi@gmail.com>
2021-06-08 00:26:26 +0530

    update

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import Details from "./Data/Details";
+import web from "./images/plus.png";
+import web1 from "./images/profile.png";
+import web2 from "./images/profile1.png";
+import web3 from "./images/profile2.png";
+import web4 from "./images/profile3.png";
 function Home() {
   const [details] = useState(Details);
 
   return (
-    <section className="bg-black py-1 px-5 position-fixed w-100 h-100">
+    <section className="section bg-black py-1 px-5 w-100">
       <div className="container-fluid w-100 h-100 B">
         <div className="row">
-          <div className="col-4 text-white">
+          <div className="col-lg-4 col-md-6 col-12 text-white">
             <div className="container mt-5">
               <div className="mb-5 card ml-3 bordercard col-2 pt-2 bg-white text-dark justify-content-center d-flex align-items-center">
-                <h1>
-                  <b>B</b>
-                </h1>
+                <h2 className="font-weight-bold text-black">B</h2>
               </div>
 
               <div className="row sidecard py-4">
@@ -41,7 +44,7 @@ function Home() {
               <div className="container justify-content-center d-flex mt-5">
                 <div className="card cardback border-0 shadow-lg fa-2x mt-5 py-3 w-75 pr-1">
                   <li className="list-unstyled">
-                    <div className="fa fa-rocket text-danger d-flex pl-4">
+                    <div className="fa fa-rocket text-danger fa-lg d-flex pl-4">
                       <h6 className="pl-4 text-white">
                         Pro account is more powerful. Get 30% off<br></br>
                         <div className="btn btn-primary bg-white text-danger font-weight-bold mt-3 px-3">
@@ -52,24 +55,50 @@ function Home() {
                   </li>
                 </div>
               </div>
-              <div className="container logout">
-                <div className="float-left mt-5 ml-4 font-weight-bold">
-                  Logout
+              <div className="container mt-4 ">
+                <div className="float-left fa fa-sign-out logout mt-5 ml-4 font-weight-bold">
+                  &nbsp;<span>Logout</span>
                 </div>
-                <div className="text-white card cardback1 col-sm float-right mt-4">
+                <div className="text-white card float-right cardback1 col-sm justify-content-end d-flex mt-4">
                   <div className="fa fa-plus fa-lg px-3 py-3 text-white"></div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-8">
-            <div className="container-fluid A py-3 my-3">
-              <div className="container py-3">
+          <div className="col-lg-8 col-md-6 col-12 mt-lg-0 mt-md-0 mt-3">
+            <div className="container-fluid A h-100 py-3 pb-4 my-3">
+              <div className="container py-2">
+                <div className="text-white float-right col-sm justify-content-end d-flex">
+                  <img
+                    src={web}
+                    className="text-danger logout d-flex w-75 img-fluid mb-2"
+                  ></img>
+</div>
+                  <div className="text-white float-lg-right float-sm-right float-md-left mt-lg-1 mb-2">
+                    <img
+                      src={web2}
+                      className="text-danger profile fa-circle fa img-fluid"
+                    ></img>
+                    <img
+                      src={web1}
+                      className="text-danger profile fa-circle fa img-fluid"
+                    ></img>
+                    <img
+                      src={web4}
+                      className="text-danger profile fa-circle fa img-fluid"
+                    ></img>
+                    <img
+                      src={web3}
+                      className="text-danger profile fa-circle fa img-fluid"
+                    ></img>
+                 
+                </div>
                 <h1 className="text-white font-weight-bold">Motivation</h1>
+
                 <ul className="nav nav-tabs">
                   <li className="active">
                     <a
-                      className="active font-weight-bolder px-2 py-2 tab rounded-0 text-decoration-none"
+                      className="active mx-3 font-weight-bolder px-2 py-2 tab rounded-0 text-decoration-none"
                       type="button"
                       data-toggle="tab"
                       href="#all"
@@ -141,25 +170,25 @@ function Home() {
                     status,
                   } = elem;
                   return (
-                    <div className="tab-content mt-5">
-                      <div id="all" className="active tab-pane fade in ">
+                    <div className="tab-content mt-4">
+                      <div id="all" className="active tab-pane">
                         <div className="row container">
-                          <div className="col-5">
+                          <div className="col-lg-5 col-12">
                             <div className="text-white py-3">
                               <div className="D card tile1">
                                 <div className="row">
-                                  <div className="col-5 justify-content-center d-flex align-items-center pl-5 pb-5 mb-5">
-                                    <h4 className="text-white font-weight-bold">
+                                  <div className=" justify-content-center d-flex align-items-center">
+                                    <div className="text-white font-weight-bold pt-4 mb-4">
                                       {tag}
-                                    </h4>
+                                    </div>
                                     <div className="play">
                                       <div className="fa fa-play px-3 text-center font-weight-bold"></div>
                                     </div>
                                   </div>
-                                  <div className="col-7">
+                                  <div className="">
                                     <img
                                       src={image}
-                                      className="tile1 img-fluid"
+                                      className="h-100 img-fluid"
                                     ></img>
                                   </div>
                                 </div>
@@ -172,23 +201,23 @@ function Home() {
                               ></img>
                             </div>
                           </div>
-                          <div className="col-7">
-                            <div className="row">
+                          <div className="col-lg-7 col-12">
+                            <div className="row tile1">
                               <div className="col-6 justify-content-center d-flex">
                                 <div className="back w-100 my-3 card text-white py-3">
-                                  <h5 className="font-weight-bold pt-1 px-3">
+                                  <div className="font-weight-bold pt-1 px-3 fsize">
                                     {tag1}
-                                  </h5>
-                                  <div className="play2 px-2">
+                                  </div>
+                                  <div className="play2 mt-lg-5 pt-lg-5 px-lg-4 px-3">
                                     <div className="fa fa-play px-3 text-center font-weight-bold"></div>
                                   </div>
                                 </div>
                               </div>
                               <div className="col-6 ">
                                 <div className="D card my-3 text-white">
-                                  <h5 className="zindex1 font-weight-bold pt-3 px-3">
+                                  <div className="zindex1 font-weight-bold pt-3 px-3">
                                     {tag2}
-                                  </h5>
+                                  </div>
                                   <img
                                     src={image1}
                                     className="img-fluid position-relative gx-0 tile"
